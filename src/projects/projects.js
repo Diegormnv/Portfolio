eb7d744e1cd0eb33b833e0141dbf0816d5a4d331
Diego Roman <diegormnv209@gmail.com>
2021-11-24 05:44:00 -0800

    testing to see if commit works and adding more projects

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -9,6 +9,36 @@ const projects = [
       "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=827&q=80",
     languages: "ReactJs Styled Components",
   },
+  {
+    id: 2,
+    title: "African Marketplace",
+    description:
+      "A web application used for users to both see what products are avilable for purchase. A user can also sign up to become a seller and put items up for sale. This was a group project consisting a total of 6 people to build this within a week. I along with one other were in charge of the state management for this project.",
+    link: "https://africanmarketplace-bwjeh2rln.vercel.app/login",
+    image:
+      "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=827&q=80",
+    languages: "ReactJs Redux",
+  },
+  {
+    id: 3,
+    title: "Secret Family Recipe",
+    description:
+      "A React app using state management and axios to fetch data from Nasa's api.",
+    link: "https://bw-super-sweet-org-leuabb06c-tt-17-super-sweet-org.vercel.app/",
+    image:
+      "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=827&q=80",
+    languages: "NodeJs PostgresQL Knex ExpressJS",
+  },
+  {
+    id: 4,
+    title: "Family Promise",
+    description:
+      "A React app using state management and axios to fetch data from Nasa's api.",
+    link: "https://nasa-photo-of-the-day-kd8bg1wfr-diegormnv.vercel.app/",
+    image:
+      "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=827&q=80",
+    languages: "ReactJs Styled Components",
+  },
 ];
 
 export default projects;
